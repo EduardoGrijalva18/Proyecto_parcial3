715c69c8fe3fc7052e221d14734d6d3b6238c699
armandosandovalesp <114783139+armandosandovalesp@users.noreply.github.com>
2022-12-04 19:58:46 -0600

    Add files via upload

---
+++ ProyectoLC/Proyecto Archivos/index.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Apple México</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <script src="js/jquery-3.6.1.js"></script>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    <?php include 'menu.php'; ?>
+
+   <section class="section home">
+      <div class="container">
+         <div class="product-home">
+            <ul class="products">
+               <li id="iphone" class="product -active">
+                  <img src="img/i14promax.png" alt="iPhone 14 PRO MAX" width="296"
+                     height="344" class="image-product">
+               </li>
+            </ul>
+         </div>
+         <div class="content-home">
+            <h1 class="title">
+               <img src="img/logo-black.svg" width="28" height="33" alt="logo Apple">
+               iPhone
+            </h1>
+            <h2 class="model">iPhone 14 Pro</h2>
+            <h3 class="slogan">Se pasa de Pro.</h3>
+            <p class="description">Una forma nueva y mágica de interactuar con tu iPhone. Una nueva funcionalidad de seguridad diseñada para salvar vidas. Y una innovadora cámara de 48 MP que logra un nivel de detalle deslumbrante. Todo gracias a la potencia del ultrarrápido chip A16 Bionic..</p>
+            <p class="value">$28,999MX</p>
+            <a href="iphone14.php" class="cta-button">Comprar</a>        
+      </div>
+   </section>
+
+   <section class="section home">
+      <div class="container">
+         <div class="product-home">
+            <ul class="products">
+               <li id="iphone" class="product -active">
+                  <img src="img/i13promax.png" alt="iPhone 13 PRO MAX" width="296"
+                     height="344" class="image-product">
+               </li>
+            </ul>
+         </div>
+         <div class="content-home">
+            <h1 class="title">
+               <img src="img/logo-black.svg" width="28" height="33" alt="logo Apple">
+               iPhone
+            </h1>
+            <h2 class="model">iPhone 13 Pro Max</h2>
+            <h3 class="slogan">Saca tu lado Pro</h3>
+            <p class="description">Un sistema de cámaras mucho más poderoso. Una pantalla con respuesta inmediata en cada interacción. El chip de smartphone más rápido del mundo. Un diseño increíblemente resistente. Y un gran salto en duración de batería.</p>
+            <p class="value">$25,999</p>
+            <a href="iphone13.php" class="cta-button">Comprar</a>
+         </div>
+      </div>
+   </section>
+
+   <section class="section home">
+      <div class="container">
+         <div class="product-home">
+            <ul class="products">
+               <li id="iphone" class="product -active">
+                  <img src="img/i12promax.png" alt="iPhone 12 PRO MAX" width="296"
+                     height="344" class="image-product">
+               </li>
+            </ul>
+         </div>
+         <div class="content-home">
+            <h1 class="title">
+               <img src="img/logo-black.svg" width="28" height="33" alt="logo Apple">
+               iPhone
+            </h1>
+            <h2 class="model">iPhone 12 Pro Max</h2>
+            <h3 class="slogan">Una experiencia 5G superior</h3>
+            <p class="description">El mejor iPhone hasta ahora viene con el poderoso chip A14 Bionic, un diseño totalmente nuevo con Ceramic Shield, sistema de cámaras Pro, escáner LiDAR, y la pantalla Super Retina XDR más grande en un iPhone hasta ahora</p>
+            <p class="value">$19,999</p>
+            <a href="iphone12.php" class="cta-button">Comprar</a>
+         </div>
+      </div>
+   </section>
+
+   <section class="section home">
+      <div class="container">
+         <div class="product-home">
+            <ul class="products">
+               <li id="iphone" class="product -active">
+                  <img src="img/macbook.png" alt="MacBook Air Pro" width="296"
+                     height="344" class="image-product">
+               </li>
+            </ul>
+         </div>
+         <div class="content-home">
+            <h1 class="title">
+               <img src="img/logo-black.svg" width="28" height="33" alt="logo Apple">
+               Mac
+            </h1>
+            <h2 class="model">MacBook Air</h2>
+            <h3 class="slogan">Tan poderosa que vuela.</h3>
+            <p class="description">El chip M2 supera el chip M1 en velocidad y eficiencia energética, y marca el inicio de una nueva era para los chips de Apple. Viene con un potente CPU de 8 núcleos para que puedas trabajar más rápido, un GPU de hasta 10 núcleos para crear imágenes y animaciones espectaculares y un motor multimedia de alto rendimiento que te permite editar más secuencias de video ProRes 4K y 8K. Además, la batería de hasta 18 horas de duración siempre te sigue el ritmo mientras trabajas o juegas.2</p>
+            <p class="value">$29,999</p>
+            <a href="macbook.php" class="cta-button">Comprar</a>
+         </div>
+      </div>
+   </section>
+
+   <section class="section home">
+      <div class="container">
+         <div class="product-home">
+            <ul class="products">
+               <li id="iphone" class="product -active">
+                  <img src="img/airpods.png" alt="AirPods Pro" width="296"
+                     height="344" class="image-product">
+               </li>
+            </ul>
+         </div>
+         <div class="content-home">
+            <h1 class="title">
+               <img src="img/logo-black.svg" width="28" height="33" alt="logo Apple">
+               AirPods
+            </h1>
+            <h2 class="model">Airpods Pro</h2>
+            <h3 class="slogan">Remasterizados por completo.</h3>
+            <p class="description">Los AirPods Pro se sometieron a un rediseño radical para alcanzar una calidad de audio todavía mejor. La Cancelación Activa de Ruido mejorada y el modo Ambiente adaptable reducen aún más el ruido exterior. El audio espacial lleva la experiencia del sonido inmersivo a un nivel mucho más personal. El control táctil ahora te permite ajustar el volumen con sólo deslizar. Y gracias a una mejora sustancial en la autonomía, ahora puedes disfrutar 6 horas de batería con una sola carga.</p>
+            <p class="value">$5,999</p>
+            <a href="airpods.php" class="cta-button">Comprar</a>
+         </div>
+      </div>
+   </section>
+
+   <section class="section home">
+      <div class="container">
+         <div class="product-home">
+            <ul class="products">
+               <li id="iphone" class="product -active">
+                  <img src="img/watchultra.png" alt="Watch Ultra" width="296"
+                     height="344" class="image-product">
+               </li>
+            </ul>
+         </div>
+         <div class="content-home">
+            <h1 class="title">
+               <img src="img/logo-black.svg" width="28" height="33" alt="logo Apple">
+               Watch
+            </h1>
+            <h2 class="model">Watch Ultra</h2>
+            <h3 class="slogan">Listo para enfrentarse al mundo.</h3>
+            <p class="description">Para crear un reloj épico y con un rendimiento sin igual, no se te puede escapar ni un detalle. El titanio ofrece el equilibrio perfecto entre peso, solidez y resistencia a la corrosión. La nueva caja sobrepasa levemente el cristal de zafiro plano protegiéndolo por los cuatro flancos. Y tanto la Digital Crown como el botón lateral han sido mejorados en tamaño y forma para que puedas usarlos sin problema incluso con guantes.</p>
+            <p class="value">$19,999</p>
+            <a href="watch.php" class="cta-button">Comprar</a>
+         </div>
+      </div>
+   </section>
+
+
+
+
+    <script src="js/bootstrap.js"></script>
+</body>
+</html>
